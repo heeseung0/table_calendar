adefb062346ef3f16f1c7927e91977791a41994f
heeseung <ys2645@naver.com>
2024-04-08 13:28:17 +0900

    모달창 수정, 일정 추가 버튼 완성 (function 추가 안됨)

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar_practice/component/custom_text_field.dart';
+import 'package:table_calendar_practice/const/colors.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
   const ScheduleBottomSheet({super.key});
@@ -10,11 +12,62 @@ class ScheduleBottomSheet extends StatefulWidget {
 class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
   @override
   Widget build(BuildContext context) {
+    final bottomInset =
+        MediaQuery.of(context).viewInsets.bottom; // 이거 추가 안하면 위젯 가려짐
+
     return SafeArea(
       child: Container(
-        height: MediaQuery.of(context).size.height / 2,
+        height: MediaQuery.of(context).size.height / 2 + bottomInset,
         color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 8,
+            right: 8,
+            top: 8,
+            bottom: bottomInset,
+          ),
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  Expanded(
+                    child: CustomTextField(
+                      label: '시작 시간',
+                      isTime: true,
+                    ),
+                  ),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: CustomTextField(
+                      label: '종료 시간',
+                      isTime: true,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              const Expanded(
+                child: CustomTextField(
+                  label: '내용',
+                  isTime: false,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onSavePressed,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                  ),
+                  child: const Text('저장'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  void onSavePressed() {}
 }
